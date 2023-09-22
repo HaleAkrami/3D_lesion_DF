187e1743c77c873589ea878d96ddeeb29a488bb8
Haleh Akrami <akrami@d11-02.hpc.usc.edu>
2023-09-22 11:35:10 -0700

    reorgnize and add normalization

--- 3d_ddpm.py
+++ 3d_ddpm.py
@@ -1,68 +1,74 @@
+
+
+import wandb
+wandb.init(project='33_ddpm',name='half_norm')
+
+
+# Standard libraries
 import os
 import tempfile
 import time
-import matplotlib.pyplot as plt
-import numpy as np
-import torch
-import torch.nn.functional as F
-from monai.apps import DecathlonDataset
-from monai.config import print_config
-from monai.data import DataLoader
-from monai.transforms import AddChanneld, CenterSpatialCropd, Compose, Lambdad, LoadImaged, Resized, ScaleIntensityd
-from monai.utils import set_determinism
-from torch.cuda.amp import GradScaler, autocast
-from tqdm import tqdm
-from torch.utils.data import DataLoader, Dataset, random_split
-import scipy
 import io
 import random
 import math
+import warnings
+from multiprocessing import Manager
+from typing import Optional
+
+# Data manipulation libraries
+import numpy as np
 import pandas as pd
+import scipy
+
+# PyTorch and related libraries
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.cuda.amp import GradScaler, autocast
+from torch.utils.data import DataLoader, Dataset, random_split
+
+# MONAI libraries
+from monai.apps import DecathlonDataset
+from monai.config import print_config
+from monai.data import DataLoader
+from monai.transforms import (
+    AddChanneld, 
+    CenterSpatialCropd, 
+    Compose, 
+    Lambdad, 
+    LoadImaged, 
+    Resized, 
+    ScaleIntensityd
+)
+from monai.utils import set_determinism
+
+# Other medical image processing libraries
+import SimpleITK as sitk
+import torchio as tio
+
+# Plotting and visualization
+import matplotlib.pyplot as plt
+
+# Progress bar
+from tqdm import tqdm
+
+# Custom modules
 from generative.inferers import DiffusionInferer
 from generative.networks.nets import DiffusionModelUNet
 from generative.networks.schedulers import DDPMScheduler, DDIMScheduler
-import numpy as np
-import SimpleITK as sitk
-import torchio as tio
-import torch.nn as nn
-sitk.ProcessObject.SetGlobalDefaultThreader("Platform")
-from multiprocessing import Manager
 
-from typing import Optional
+# Weights and Biases for experiment tracking
+import wandb
+from dataloader import Train ,Eval 
 
-import warnings
-warnings.filterwarnings('ignore')
-import os
-torch.cuda.empty_cache()
-# Set the CUDA_VISIBLE_DEVICES environment variable to specify the GPU device
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7'
-# Set the manualSeed, random seed, and device
-manualSeed = 999
-random.seed(manualSeed)
-torch.manual_seed(manualSeed)
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# It's recommended to set the global deterministic behavior of some libraries.
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
 
-# Set the default number of threads for SimpleITK.
-sitk.ProcessObject.SetGlobalDefaultThreader("Platform")
 
-# Set the global behavior of SimpleITK to use the Platform multithreading.
-# This is especially useful when using SimpleITK with multi-core systems.
-sitk.ProcessObject.SetGlobalDefaultThreader("Platform")
-
-def normalize_to_neg_one_to_one(img):
-    return img * 2 - 1
-
-def unnormalize_to_zero_to_one(t):
-    return (t + 1) * 0.5
-
-# Replace 'path_to_your_nii_file.nii' with the actual path to your NIfTI image file
 
 
 config = {
+    'n_epochs' : 1000,
+    'val_interval' : 5,
     'batch_size': 4,
     'imgDimResize':(160,192,160),
     'imgDimPad': (208, 256, 208),
@@ -70,153 +76,20 @@ config = {
     'unisotropic_sampling': True, 
     'perc_low': 1, 
     'perc_high': 99,
-    'rescaleFactor':1,
+    'rescaleFactor':2,
     'base_path': '/scratch1/akrami/Latest_Data/Data',
 }
 
-
-def Train(csv,cfg,preload=True):
-    subjects = []
-    base_path = cfg.get('base_path',None)
-    h, w, d = tuple(cfg.get('imgDimResize',(160,192,160)))
-    for _, sub in csv.iterrows():
-       # print(sub.img_path)
-        subject_dict = {
-            'vol' : tio.ScalarImage(sub.img_path, reader=sitk_reader), 
-            'age' : sub.age,
-            'ID' : sub.img_name,
-            'path' : sub.img_path
-        }
-
-        if sub.mask_path != None: # if we have masks
-            subject_dict['mask'] = tio.LabelMap(sub.mask_path,reader=sitk_reader)
-        subject = tio.Subject(subject_dict)
-        subjects.append(subject)
-    
-    if preload: 
-        manager = Manager()
-        cache = DatasetCache(manager)
-        ds = tio.SubjectsDataset(subjects, transform = get_transform(cfg))
-        ds = preload_wrapper(ds, cache, augment = get_augment(cfg))
-    else: 
-        ds = tio.SubjectsDataset(subjects, transform = tio.Compose([get_transform(cfg),get_augment(cfg)]))
-        
-    if cfg.get('spatialDims') == '2D':
-        slice_ind = cfg.get('startslice',None) 
-        seq_slices = cfg.get('sequentialslices',None) 
-        ds = vol2slice(ds,cfg,slice=slice_ind,seq_slices=seq_slices)
-    return ds
-
-def sitk_reader(path):
-                
-    image_nii = sitk.ReadImage(str(path), sitk.sitkFloat32)
-    vol = sitk.GetArrayFromImage(image_nii).transpose(2,1,0)
-    return vol, None
-
-
-def get_transform(cfg): # only transforms that are applied once before preloading
-    exclude_from_resampling = None
-    h, w, d = tuple(cfg.get('imgDimResize',(160,192,160)))
-    if cfg.get('unisotropic_sampling',True):
-        preprocess = tio.Compose([
-        tio.CropOrPad((h,w,d),padding_mode=0),
-        tio.RescaleIntensity((0, 1),percentiles=(cfg.get('perc_low',1),cfg.get('perc_high',99)),masking_method='mask'),
-        tio.Resample(cfg.get('rescaleFactor',3.0),image_interpolation='bspline',exclude=exclude_from_resampling),#,exclude=['vol_orig','mask_orig','seg_orig']), # we do not want to resize *_orig volumes
-        ])
-
-    else: 
-        preprocess = tio.Compose([
-                tio.RescaleIntensity((0, 1),percentiles=(cfg.get('perc_low',1),cfg.get('perc_high',99))),
-                tio.Resample(cfg.get('rescaleFactor',3.0),image_interpolation='bspline',exclude=exclude_from_resampling),#,exclude=['vol_orig','mask_orig','seg_orig']), # we do not want to resize *_orig volumes
-            ])
-
-
-    return preprocess
-
-
-class DatasetCache(object):
-    def __init__(self, manager, use_cache=True):
-        self.use_cache = use_cache
-        self.manager = manager
-        self._dict = manager.dict()
-
-    def is_cached(self, key):
-        if not self.use_cache:
-            return False
-        return str(key) in self._dict
-
-    def reset(self):
-        self._dict.clear()
-
-    def get(self, key):
-        if not self.use_cache:
-            raise AttributeError('Data caching is disabled and get funciton is unavailable! Check your config.')
-        return self._dict[str(key)]
-
-    def cache(self, key, subject):
-        # only store if full data in memory is enabled
-        if not self.use_cache:
-            return
-        # only store if not already cached
-        if str(key) in self._dict:
-            return
-        self._dict[str(key)] = (subject)
-
-class preload_wrapper(Dataset):
-    def __init__(self,ds,cache,augment=None):
-            self.cache = cache
-            self.ds = ds
-            self.augment = augment
-    def reset_memory(self):
-        self.cache.reset()
-    def __len__(self):
-            return len(self.ds)
-            
-    def __getitem__(self, index):
-        if self.cache.is_cached(index) :
-            subject = self.cache.get(index)
-        else:
-            subject = self.ds.__getitem__(index)
-            self.cache.cache(index, subject)
-        if self.augment:
-            subject = self.augment(subject)
-        return subject
-    
-class vol2slice(Dataset):
-    def __init__(self,ds,cfg,onlyBrain=False,slice=None,seq_slices=None):
-            self.ds = ds
-            self.onlyBrain = onlyBrain
-            self.slice = slice
-            self.seq_slices = seq_slices
-            self.counter = 0 
-            self.ind = None
-            self.cfg = cfg
-
-    def __len__(self):
-            return len(self.ds)
-            
-    def __getitem__(self, index):
-        subject = self.ds.__getitem__(index)
-        subject['vol'].data = subject['vol'].data.permute(0,3, 2, 1)
-
-        return subject
-
-def get_augment(cfg): # augmentations that may change every epoch
-    augmentations = []
-
-    # individual augmentations
-    augment = tio.Compose(augmentations)
-    return augment
-
+wandb.config.update(config)
 
 
 imgpath = {}
 # '/acmenas/hakrami/patched-Diffusion-Models-UAD/Data/splits/BioBank_train.csv'
 #'/acmenas/hakrami/patched-Diffusion-Models-UAD/Data/splits/IXI_train_fold0.csv',
-csvpath_trains = ['/acmenas/hakrami/patched-Diffusion-Models-UAD/Data/splits/BioBank_train.csv']
-pathBase = '/acmenas/hakrami/patched-Diffusion-Models-UAD/Data_train'
-csvpath_val = '/acmenas/hakrami/patched-Diffusion-Models-UAD/Data/splits/IXI_val_fold0.csv'
-csvpath_test = '/acmenas/hakrami/patched-Diffusion-Models-UAD/Data/splits/Brats21_test.csv'
+csvpath_trains = ['/project/ajoshi_27/akrami/patched-Diffusion-Models-UAD/Data/splits/IXI_train_fold0.csv',]
+pathBase = '/scratch1/akrami/Data_train'
+csvpath_val = '/project/ajoshi_27/akrami/patched-Diffusion-Models-UAD/Data/splits/IXI_val_fold0.csv'
+csvpath_test = '/project/ajoshi_27/akrami/patched-Diffusion-Models-UAD/Data/splits/Brats21_sub_test.csv'
 var_csv = {}
 states = ['train','val','test']
 
@@ -232,8 +105,7 @@ for csvpath in csvpath_trains:
 var_csv['train'] = pd.concat(df_list, ignore_index=True)
 var_csv['val'] = pd.read_csv(csvpath_val)
 var_csv['test'] = pd.read_csv(csvpath_test)
-# if cfg.mode == 't2':
-#     keep_t2 = pd.read_csv(cfg.path.IXI.keep_t2) # only keep t2 images that have a t1 counterpart
+
 
 for state in states:
     var_csv[state]['settype'] = state
@@ -244,14 +116,12 @@ for state in states:
     else:
         var_csv[state]['seg_path'] = pathBase  + var_csv[state]['seg_path']
 
-    # if cfg.mode == 't2': 
-    #     var_csv[state] =var_csv[state][var_csv[state].img_name.isin(keep_t2['0'].str.replace('t2','t1'))]
-    #     var_csv[state]['img_path'] = var_csv[state]['img_path'].str.replace('t1','t2')
+  
     
     
 data_train = Train(var_csv['train'],config) 
 data_val = Train(var_csv['val'],config)                
-data_test = Train(var_csv['test'],config)
+data_test = Eval(var_csv['test'],config)
 
 
 
@@ -277,6 +147,10 @@ model = DiffusionModelUNet(
     num_res_blocks=2,
 )
 model.to(device)
+model_filename = '/scratch1/akrami/models/3Ddiffusion/half/model_epoch984.pt'
+
+# load state_dict into the model
+model.load_state_dict(torch.load(model_filename))
 if torch.cuda.device_count() > 1:
     print("Using", torch.cuda.device_count(), "GPUs!")
     model = nn.DataParallel(model)
@@ -287,13 +161,16 @@ inferer = DiffusionInferer(scheduler)
 optimizer = torch.optim.Adam(params=model.parameters(), lr=5e-5)
 
 
-n_epochs = 500
-val_interval = 25
+
 epoch_loss_list = []
 val_epoch_loss_list = []
 
 scaler = GradScaler()
 total_start = time.time()
+n_epochs = config.get('n_epochs',100)
+val_interval =config.get('val_interval',5)
+
+wandb.watch(model, log_freq=100)
 for epoch in range(n_epochs):
     model.train()
     epoch_loss = 0
@@ -302,6 +179,7 @@ for epoch in range(n_epochs):
     for step, batch in progress_bar:
        # images = batch["image"].to(device)
         images = batch['vol']['data'].to(device)
+        images  = torch.rand(1).item()* images
         optimizer.zero_grad(set_to_none=True)
 
         with autocast(enabled=True):
@@ -326,6 +204,8 @@ for epoch in range(n_epochs):
 
         progress_bar.set_postfix({"loss": epoch_loss / (step + 1)})
     epoch_loss_list.append(epoch_loss / (step + 1))
+    wandb.log({"loss_train": epoch_loss / (step + 1)},step=epoch)
+
 
     if (epoch + 1) % val_interval == 0:
         model.eval()
@@ -346,6 +226,7 @@ for epoch in range(n_epochs):
             val_epoch_loss += val_loss.item()
             progress_bar.set_postfix({"val_loss": val_epoch_loss / (step + 1)})
         val_epoch_loss_list.append(val_epoch_loss / (step + 1))
+        wandb.log({"loss_val": val_epoch_loss / (step + 1)},step=epoch)
 
         # Sampling image during training
         #80, 96, 80
@@ -355,17 +236,20 @@ for epoch in range(n_epochs):
         with autocast(enabled=True):
             image = inferer.sample(input_noise=image, diffusion_model=model, scheduler=scheduler)
 
+
+        middle_slice_idx = image.size(-1) // 2
         plt.figure(figsize=(2, 2))
-        plt.imshow(image[0, 0, :, :, 20].cpu(), vmin=0, vmax=1, cmap="gray")
+        plt.imshow(image[0, 0, :, :, middle_slice_idx].cpu(), vmin=0, vmax=1, cmap="gray")
         plt.tight_layout()
         plt.axis("off")
         plt.show()
+        wandb.log({"sample_image": [wandb.Image(plt)]},step=epoch)
         # Modify the filename to include the epoch number
-        filename = f"./results/test/sample_epoch{epoch}.png"
+        filename = f"./results/half_norm/sample_epoch{epoch}.png"
 
         plt.savefig(filename, dpi=300)  
         # Save the model
-        model_filename = f"./models/test/model_epoch{epoch}.pt"
+        model_filename = f"/scratch1/akrami/models/3Ddiffusion/half_norm/model_epoch{epoch}.pt"
         torch.save(model.state_dict(), model_filename)
 
 total_time = time.time() - total_start
